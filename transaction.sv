0be916ea93eeca953d95e55a9efcd3ea29acb927
Ishfaq Ahmed <34355989+ishfaqahmed29@users.noreply.github.com>
2021-08-29 21:16:56 -0400

    Updates

--- transaction.sv
+++ transaction.sv
@@ -7,6 +7,7 @@ class data_item extends uvm_sequence_item;
   rand logic 		        read_en;
   rand logic 		        write_en;
   rand logic [31:0]     data_in;
+  rand logic [31:0]     data_out;
   
   function new(string name = "data_item", uvm_component parent);
     super.new(name);
@@ -16,6 +17,7 @@ class data_item extends uvm_sequence_item;
     `uvm_field_int(read_en, UVM_DEFAULT)
   	`uvm_field_int(write_en, UVM_DEFAULT)
     `uvm_field_int(data_in, UVM_DEFAULT)
+    `uvm_field_int(data_out, UVM_DEFAULT)
   `uvm_object_utils_end
   
 endclass: data_item
